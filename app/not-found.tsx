ebd615ec8f6c2ea70ca4e4b907bcafc85cda14ac
Alex Kontis <alex@alexkontis.co.uk>
2025-05-13 10:57:40 +0100

    basic 404 page placeholder

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+export default function Custom404() {
+    return (
+        <>
+            <main className="text-center m-4 mb-12 p-6">
+                <h1 className="text-4xl md:text-5xl uppercase font-extrabold mb-4">404!</h1>
+                <h1 className="text-2xl md:text-3xl font-extrabold mb-2">Looks like you took a wrong turn</h1>
+                <Link href="/" className="text-xl underline">Let's head back home</Link>
+            </main>
+
+        </>
+    )
+}
