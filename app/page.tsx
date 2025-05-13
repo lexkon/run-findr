e8e002a7013cf661f96b97f2afe25346410c1561
Alex Kontis <alex@alexkontis.co.uk>
2025-05-13 15:27:45 +0100

    removes boilerplate svg files

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-center uppercase heading-font tracking-wide text-5xl font-extrabold text-amber-500 m-6">Run Findr</h1 >
+      <h1 className="text-left uppercase heading-font tracking-wide text-5xl font-extrabold text-[#7C3AED] m-6">Run Findr</h1 >
 
     </>
   );
