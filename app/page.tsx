f77252bd9a083106e9759eb5fa2a260cdb26e0a3
Alex Kontis <alex@alexkontis.co.uk>
2025-05-13 11:52:27 +0100

    font imports and setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-center uppercase text-5xl font-extrabold text-amber-500 m-6">Run Findr</h1>
+      <h1 className="text-center uppercase heading-font tracking-wide text-5xl font-extrabold text-amber-500 m-6">Run Findr</h1 >
+
     </>
   );
 }
